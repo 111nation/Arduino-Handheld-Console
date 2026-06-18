30a73bbc04f64e742bdb6d8458f58f6e7e7c7c20
111nation <tafara.k@outlook.com>
2026-06-18 19:50:35 +0200

    Player is able to draw when moving cursor. TODO: Optimize displaying and input lag, changing colors and erasing

--- GameEngine/game.py
+++ GameEngine/game.py
@@ -1,11 +1,12 @@
 import pygame
 import hardware
 
+BACKGROUND = "black"
 
 class Player:
     def __init__(self):
-        self.x = 240/2
-        self.y = 240/2
+        self.x = int(240/2)
+        self.y = int(240/2)
         self.size = 20
         self.thickness = 2
         self.velocity = 3
@@ -21,13 +22,13 @@ class Player:
 
         if self.x < 0:
             self.x = 0
-        elif self.x > 240:
-            self.x = 240
+        elif self.x >= 240:
+            self.x = 239
 
         if self.y < 0:
             self.y = 0
-        elif self.y > 240:
-            self.y = 240
+        elif self.y >= 240:
+            self.y = 239
 
     def draw(self, screen):
         # Draw cross hair
@@ -35,21 +36,36 @@ class Player:
         pygame.draw.rect(screen, "red", (self.x-self.size/2, self.y-self.thickness/2, self.size, self.thickness))
 
 
-player = Player()
 
 class Canvas:
     def __init__(self):
-        rows, cols = (240, 240)
-        self.canvas = [[""]*cols]*rows # 2D Matrix of pixels 
+        rows, cols = 240, 240
+        self.canvas = [["" for _ in range(cols)] for _ in range(rows)] # 2D Matrix of pixels 
 
     def draw(self, x, y, color):
-        self.canvas[x][y] = color
+        self.canvas[int(y)][int(x)] = color
+    
+    def erase(self, x, y):
+        self.canvas[int(y)][int(x)] = ""
 
+    def display(self, screen):
+        for row in range(len(self.canvas)):
+            for col in range(len(self.canvas[row])):
+                color = self.canvas[row][col]
+                pixel= (col, row, 2, 2)
+                if color == "":
+                    pygame.draw.rect(screen, BACKGROUND, pixel) 
+                else:
+                    pygame.draw.rect(screen, color, pixel) 
 
+player = Player()
+canvas = Canvas()
 
 def main(screen):
     player.move((hardware.control.joystick.x/100.0) * player.velocity,
                 (hardware.control.joystick.y/100.0) * player.velocity)
 
-    screen.fill("black")
+    screen.fill(BACKGROUND)
+    canvas.draw(player.x, player.y, "white")
+    canvas.display(screen)
     player.draw(screen)

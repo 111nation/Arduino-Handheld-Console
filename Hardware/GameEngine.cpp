435811b594ee1897b2e148605589fa2161a3975f
111nation <tafara.k@outlook.com>
2026-06-20 23:54:58 +0200

    Defined write and read operations into heap memory, added debugging functions for heap memory and updated readFile

--- Hardware/GameEngine.cpp
+++ Hardware/GameEngine.cpp
@@ -1,6 +1,95 @@
 #include "GameEngine.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <filesystem>
+
+// UNIVERSAL IMPLEMENTATION
 
 INTEGER* Heap = new INTEGER[HEAP_SIZE];
 
-void loadFile(std::string fileName) {
+bool validAddress(ADDR address) {
+	return address >= 0 && address < HEAP_SIZE;
 }
+
+void write(ADDR address, INTEGER value) {
+	// Write value at specified heap address
+	if (!validAddress(address)) return;
+	Heap[address] = value;
+}
+
+INTEGER read(ADDR address) {
+	// Return value at specified heap address
+	return validAddress(address) ? Heap[address] : 0;
+}
+
+void parse(STRING line) {
+}
+
+#ifdef EMULATE 
+
+using namespace std;
+
+string readFile(std::string fileName) {
+	// Read file and return its string data
+	ifstream file(fileName);
+	string line, contents;	
+
+	// Read File Contents
+	while (getline(file, line)) {
+		// Skip leading whitespace
+		size_t start = line.find_first_not_of(" \t\n\r\f\v");
+
+		// Skip comments and empty lines
+		if (start == std::string::npos || line[start] == INLINE_COMMENT) continue;
+
+		contents += line + '\n';
+	}
+
+	file.close();
+
+	return contents;
+}
+
+void initDebugHeap() {
+	// Set all heap values to "null" by setting a large negative number
+	for (int i = 0; i < HEAP_SIZE; i++) {
+		Heap[i] = NULL_INT;
+	}
+}
+
+void printHeap() {
+	// Print heap and skip empty contents
+	cout << "Heap:\t[ ";
+	
+	int occupied = 0;
+	bool allowEllips = true;
+	for (int i = 0; i < HEAP_SIZE; i++) {
+		if (Heap[i] == NULL_INT) {
+			if (allowEllips) cout << "... , ";
+			allowEllips = false;
+			continue;
+		} 
+
+		cout << Heap[i] << " , ";
+		allowEllips = true;
+	}
+
+	cout << "\b\b ]\n";
+}
+
+int main() {
+	string program = readFile("programs/main");
+	initDebugHeap();
+
+	write(6, 30);
+	write(7, -1);
+	write(3, read(6));
+
+	printHeap();
+	return 0;
+}
+
+#else
+
+#endif 
--- Hardware/GameEngine.hpp
+++ Hardware/GameEngine.hpp
@@ -1,10 +1,35 @@
 #ifndef GAME_ENGINE_HPP
 #define GAME_ENGINE_HPP
 
+#include <cstdint>
+
+// DATA TYPES
+
+#define STRING const char*
 #define INTEGER short int // 16 bit integer
+#define ADDR uint8_t
 #define HEAP_SIZE 64      // 64 heap memory addresses
 
+// SYNTAX CONSTANTS
+constexpr char INLINE_COMMENT = '#';
+
+// ========================
+// UNIVERSAL IMPLEMENTATION
+// ========================
+
 extern INTEGER* Heap;     // Heap memory
+
+// Helpers
+bool validAddress(ADDR address);
+STRING trimLeadingTrailingWhitespace(STRING value);
+
+// Basic I/O
+void write(ADDR address, INTEGER value);
+INTEGER read(ADDR address);
+
+// Parsing and Interpreting
+void parse(STRING line); 
+
 
 #define EMULATE
 						
@@ -12,12 +37,20 @@ extern INTEGER* Heap;     // Heap memory
  * SWITCH BETWEEN EMULATING LANGUAGE ON 
  * PERSONAL COMPUTER AND ARDUINO HARDWARE
  */
-#ifndef EMULATE 
+#ifdef EMULATE 
 	#include <iostream>
 	#include <fstream>
 	#include <string>
+	
+	constexpr INTEGER NULL_INT = 1 << (sizeof(INTEGER) * 8 - 1);
 
-	void loadFile(std::string fileName);
+	using namespace std;
+
+	string readFile(string fileName);
+
+	// Debug Functions
+	void initDebugHeap();
+	void printHeap();
 #else
 
 #endif 

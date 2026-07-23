7089f7c93be1075e4c98761626a25925de08649c
111nation <tafara.k@outlook.com>
2026-07-23 22:44:20 +0200

    Experimental: Initialize correct CWD of executable in MacOS

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -2,6 +2,17 @@
 #include "Parse.hpp"
 #include "Debug.hpp"
 
+#ifdef __APPLE__
+	#include <TargetConditionals.h>
+#endif
+
+#if defined(__APPLE__) && TARGET_OS_MAC 
+	#include <mach-o/dyld.h>
+	#include <limits.h>
+	#include <libgen.h>
+	#include <unistd.h>
+#endif
+
 void interpret() {
 	if (!isRunning) return;
 
@@ -27,7 +38,30 @@ void interpret() {
         }
     }
 
+		bool changeWorkingDirectory() {
+			#ifdef TARGET_OS_MAC 
+				char buf[PATH_MAX];
+				uint32_t bufsize = PATH_MAX;
+				if (_NSGetExecutablePath(buf, &bufsize)==-1) return false;
+
+				char* dir = dirname(buf);
+
+				// Failed to change directory
+				if (chdir(dir) != 0) {
+					return false;
+				}
+			#endif
+
+			return true;
+		}
+
 	int main() {
+		if (!changeWorkingDirectory()) {
+			std::cout << "Failed to initialize game emulator\n";
+			std::cout << "Could not change current working directory\n";
+			return 0;
+		}
+
 		init("programs/main", "/dev/ttyACM0");
 		
 		initDebugHeap();
